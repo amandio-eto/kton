9beb3ae11d0247b76853e1000a26566ea6cb714e
unknown <JoseAmandiodeAlmeida021099@gmail.com>
2022-06-28 01:29:03 +0900

    update php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="google-site-verification" content="RzGS-0pbq4Vz69kFrVBSgEQmeINdcaNmd7iF-ehvTOw" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">KTON</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contas Us</a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
+    <div class="jumbotron">
+        <h1 class="display-4">Hello Word</h1>
+        <p class="lead">this website is under development by the IT Esperanca Group</p>
+        <hr class="my-4">
+        <p>For complete information, please contact this email amandio.almeida@eto.tl Thank you very much.</p>
+        <footer>Copy Right &copy; By ICT Esperanca Group Timor-Leste</footer>
+    </div>
